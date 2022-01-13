de183ce202aa95d078cb941395fb557ccb96b729
Rohan Mitchell <rohan@rohanmitchell.com>
2022-01-13 15:44:46 +1100

    Move package setup to its own file

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 (load (expand-file-name "better-defaults.el" user-emacs-directory))
 (load (expand-file-name "keys.el" user-emacs-directory))
 (load (expand-file-name "packages.el" user-emacs-directory))
+(load (expand-file-name "package-setup.el" user-emacs-directory))
 
 
 ;; Override some better-defaults
@@ -22,25 +23,4 @@
 ;; Delete trailing whitespace
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-;; multiple-cursors
-(require 'multiple-cursors)
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C->") 'mc/mark-all-like-this)
 
-;; yasnippet
-(require 'yasnippet)
-(yas-global-mode 1)
-(yas/load-directory "~/.emacs.d/snippets")
-;;;; No dropdowns please, yas
-;;(setq yas-prompt-functions '(yas-ido-prompt yas-completing-prompt))
-;;;; No need to be so verbose
-;;(setq yas-verbosity 1)
-;;;; Wrap around region
-;;(setq yas-wrap-around-region t)
-
-;; line numbers
-(if (fboundp 'global-display-line-numbers-mode)
-    (global-display-line-numbers-mode)
-  (global-nlinum-mode t))
---
+++ package-setup.el
@@ -0,0 +1,22 @@
+;; multiple-cursors
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C->") 'mc/mark-all-like-this)
+
+;; yasnippet
+(require 'yasnippet)
+(yas-global-mode 1)
+(yas/load-directory "~/.emacs.d/snippets")
+;;;; No dropdowns please, yas
+;;(setq yas-prompt-functions '(yas-ido-prompt yas-completing-prompt))
+;;;; No need to be so verbose
+;;(setq yas-verbosity 1)
+;;;; Wrap around region
+;;(setq yas-wrap-around-region t)
+
+;; line numbers
+(if (fboundp 'global-display-line-numbers-mode)
+    (global-display-line-numbers-mode)
+  (global-nlinum-mode t))
