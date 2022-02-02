b6325178e7e0af394ba5e0fe852cf7bd28b78ced
Rohan Mitchell <rohan@rohanmitchell.com>
2022-02-03 09:42:39 +1100

    Reinstate selected packages

--- custom.el
+++ custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(scala-mode zop-to-char zenburn-theme yasnippet-snippets yaml-mode scss-mode prettier nlinum multiple-cursors markdown-mode magit json-mode flycheck expand-region exec-path-from-shell enh-ruby-mode editorconfig easy-kill dockerfile-mode diff-hl crux browse-kill-ring anzu ag))
  '(safe-local-variable-values '((eval prettier-mode t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
